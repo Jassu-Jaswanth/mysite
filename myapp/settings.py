c44110a93f6d6d0ee1d2f0a86889d93742969718
Jassu-Jaswanth <alliswelljaswanth@gmail.com>
2021-09-19 13:07:17 +0530

    Changed env variable names

--- myapp/settings.py
+++ myapp/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USERNAME': os.environ['USERNAME'],
-        'PASSWORD': os.environ['PASSWORD'],
-        'HOST': os.environ['HOST'],
-        'PORT': os.environ['PORT'],
+        'NAME': os.environ['POSTGRES_DB_NAME'],
+        'USERNAME': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT'],
     }
 }
 
