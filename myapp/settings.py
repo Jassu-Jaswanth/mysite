69b6bbae98c4f269262e1422e9d46c592f54de09
Jassu-Jaswanth <alliswelljaswanth@gmail.com>
2021-09-19 12:42:06 +0530

    some random corrections

--- myapp/settings.py
+++ myapp/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mysterious-spire-33222.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','mysterious-spire-33222.herokuapp.com']
 
 
 # Application definition
